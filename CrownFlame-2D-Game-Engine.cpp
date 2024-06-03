8452213bd12007e584224f3e2d702d76e593f356
DNofulla <=>
2024-06-03 02:27:39 -0500

    Initialized repository with CMake and Visual Studio

---
+++ CrownFlame-2D-Game-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// CrownFlame-2D-Game-Engine.cpp : Defines the entry point for the application.
+//
+
+#include "CrownFlame-2D-Game-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
